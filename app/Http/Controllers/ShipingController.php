7a831050dc47cc86b2c2183b2bbad6f4635bad7f
opeshamz <Samxyn007@yahoo.com>
2021-05-24 05:23:54 +0100

    Update ShipingController.php

--- app/Http/Controllers/ShipingController.php
+++ app/Http/Controllers/ShipingController.php
@@ -88,30 +88,6 @@ class ShipingController extends Controller
 
     public function payment(Request $request)
     {
-        $url = "https://api.paystack.co/transaction";
-        $fields = [
-            'email' => "customer@email.com",
-            'amount' => '2000000',
-
-        ];
-        $fields_string = http_build_query($fields);
-        //open connection
-        $ch = curl_init();
-
-        //set the url, number of POST vars, POST data
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            "Authorization: Bearer sk_test_42c41abe67f20b48b853a52e310297e13008c5cf",
-            "Cache-Control: no-cache",
-        ));
-
-        //So that curl_exec returns the contents of the cURL; rather than echoing it
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-        //execute post
-        $result = curl_exec($ch);
-        echo  $result;
+        
     }
 }
